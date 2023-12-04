2f61375cb80dbca807812c0db9ee7b705fcf7030
Derek Lettman <dlettman@auditboard.com>
2023-12-04 09:12:03 -0800

    bit of cleanup on day 4

--- 04/exercise_04.py
+++ 04/exercise_04.py
@@ -14,8 +14,8 @@ def parse_cards(input):
         card_num = card_num.split()[1]
         winning_nums, your_nums = nums.split(" | ")
         cards[card_num] = {
-            "winning_nums": [int(num) for num in winning_nums.strip().split()],
-            "your_nums": [int(num) for num in your_nums.strip().split()],
+            "winning_nums": set([int(num) for num in winning_nums.strip().split()]),
+            "your_nums": set([int(num) for num in your_nums.strip().split()]),
         }
     return cards
 
@@ -26,9 +26,8 @@ def part_one(input_filename):
     cards = parse_cards(input)
     for card in cards:
         card_score = 0.5
-        for winning_number in cards[card]["winning_nums"]:
-            if winning_number in cards[card]["your_nums"]:
-                card_score *= 2
+        for _ in cards[card]["winning_nums"].intersection(cards[card]["your_nums"]):
+            card_score *= 2
         score += floor(card_score)
     return score
 
@@ -37,15 +36,14 @@ def part_two(input_filename):
     input = helpers.parse_input(input_filename)
     cards = parse_cards(input)
     card_count = {str(n + 1): 1 for n in range(len(input))}
-    for idx, card in enumerate(cards):
+    for card in cards:
         card_multiplier = card_count[card]
-        card_score = 0
-        card_score += sum([1 for num in cards[card]["winning_nums"] if num in cards[card]["your_nums"]])
+        card_score = len(cards[card]["winning_nums"].intersection(cards[card]["your_nums"]))
         next_card = int(card) + 1
         for n in range(card_score):
             card_count[str(next_card)] += card_multiplier
             next_card += 1
-    return sum([n for n in card_count.values()])
+    return sum(card_count.values())
 
 
 if __name__ == "__main__":
