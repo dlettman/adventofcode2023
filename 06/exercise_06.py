bad6a09e4c210a3f8067c3592ff0c30153e64a90
Derek Lettman <dlettman@auditboard.com>
2023-12-05 21:14:00 -0800

    solve day 6

--- 06/exercise_06.py
+++ 06/exercise_06.py
@@ -1,22 +1,30 @@
 import sys
+import math
 
 sys.path.append("..")
 from helpers import helpers
 import pyperclip
 
 
+def parse_lines(input):
+    times, distances = [[int(item) for item in input[n].split(": ")[1].strip().split()] for n in range(2)]
+    return times, distances
+
+
 def part_one(input_filename):
+    scores = []
     input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    times, distances = parse_lines(input)
+    for time, distance in zip(times, distances):
+        scores.append(sum([1 for n in range(time) if (((time - n) * n) > distance)]))
+    return math.prod(scores)
 
 
 def part_two(input_filename):
     input = helpers.parse_input(input_filename)
-    # do stuff here
-    output = None
-    return output
+    time, distance = [int("".join(input[n].split(": ")[1].strip().split())) for n in range(2)]
+    score = sum([1 for n in range(time) if (((time - n) * n) > distance)])
+    return score
 
 
 if __name__ == "__main__":
@@ -28,7 +36,7 @@ if __name__ == "__main__":
     print(f"Test result = {part_two('inputtest.txt')}\n")
     p2result = part_two("input.txt")
     print(f"REAL RESULT = {p2result}")
-    if p1result:
-        pyperclip.copy(p1result)
-    elif p2result:
+    if p2result:
         pyperclip.copy(p2result)
+    elif p1result:
+        pyperclip.copy(p1result)
