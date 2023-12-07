acf5e39017adbb8750fed43384110624d81ecffc
Derek Lettman <dlettman@auditboard.com>
2023-12-07 09:34:34 -0800

    bit of cleanup day 7

--- 07/exercise_07.py
+++ 07/exercise_07.py
@@ -5,105 +5,85 @@ from helpers import helpers
 import pyperclip
 from collections import Counter
 
-RANKS = list("AKQJT98765432")
+RANKS = "AKQJT98765432"
+RANK_DICT = {char: idx for idx, char in enumerate(RANKS)}
 
-RANKS2 = list("AKQT98765432J")
-
-
-def sort_tier(tier):
-    return sorted(tier, key=lambda x: [RANKS.index(c) for c in x[0]])
+RANKS2 = "AKQT98765432J"
+RANK2_DICT = {char: idx for idx, char in enumerate(RANKS2)}
 
 
-def sort_tier2(tier):
-    return sorted(tier, key=lambda x: [RANKS2.index(c) for c in x[0]])
+def parse_hand(line):
+    hand, bid = line.split()
+    bid = int(bid)
+    card_count = Counter(hand)
+    sorted_cards = sorted(card_count, key=lambda x: card_count[x], reverse=True)
+    return hand, bid, card_count, sorted_cards
+
+
+def make_tiers(lines, part_2=False):
+    tiers = {"5": [], "4": [], "FH": [], "3": [], "2P": [], "1P": [], "HC": []}
+    for line in lines:
+        hand, bid, card_count, sorted_cards = parse_hand(line)
+        card = sorted_cards[0]
+        if part_2:
+            if "J" in card_count:
+                if card != "J":
+                    card_count[card] += card_count["J"]
+                else:  # card == J
+                    try:
+                        card = sorted_cards[1]
+                        card_count[card] += card_count["J"]
+                        card_count["J"] = 0
+                    except IndexError:  # All Js, baby!
+                        pass
+        if card_count[card] == 5:
+            tiers["5"].append((hand, int(bid)))
+        elif card_count[card] == 4:
+            tiers["4"].append((hand, int(bid)))
+        elif card_count[card] == 3:
+            card2 = sorted_cards[1]
+            if card_count[card2] == 2:
+                tiers["FH"].append((hand, int(bid)))
+            else:
+                tiers["3"].append((hand, int(bid)))
+        elif card_count[card] == 2:
+            card2 = sorted(card_count, key=lambda x: card_count[x], reverse=True)[1]
+            if card_count[card2] == 2:
+                tiers["2P"].append((hand, int(bid)))
+            else:
+                tiers["1P"].append((hand, int(bid)))
+        else:
+            tiers["HC"].append((hand, int(bid)))
+    return tiers
+
+
+def sort_tier(tier, part_2=False):
+    key_dict = RANK2_DICT if part_2 else RANK_DICT
+    return sorted(tier, key=lambda x: [key_dict[c] for c in x[0]])
+
+
+def make_ranking(tiers, part_2=False):
+    ranked = []
+    for tier in ["5", "4", "FH", "3", "2P", "1P", "HC"]:
+        ranked += sort_tier(tiers[tier], part_2=part_2)
+    ranked.reverse()
+    return ranked
+
+
+def do_it(input_filename, part_2=False):
+    input = helpers.parse_input(input_filename)
+    tiers = make_tiers(input, part_2=part_2)
+    ranked = make_ranking(tiers, part_2=part_2)
+    score = sum([(idx + 1) * hand[1] for idx, hand in enumerate(ranked)])
+    return score
 
 
 def part_one(input_filename):
-    input = helpers.parse_input(input_filename)
-    ranked = []
-    tiers = {"5": [], "4": [], "FH": [], "3": [], "2P": [], "1P": [], "HC": []}
-    for line in input:
-        hand = line.split()[0]
-        card_count = Counter(line.split()[0])
-        bid = int(line.split()[1])
-        card = sorted(card_count, key=lambda x: card_count[x], reverse=True)[0]
-        if card_count[card] == 5:
-            tiers["5"].append((hand, int(bid)))
-        elif card_count[card] == 4:
-            tiers["4"].append((hand, int(bid)))
-        elif card_count[card] == 3:
-            fh = False
-            for card2 in card_count:
-                if card_count[card2] == 2:
-                    tiers["FH"].append((hand, int(bid)))
-                    fh = True
-            if not fh:
-                tiers["3"].append((hand, int(bid)))
-        elif card_count[card] == 2:
-            TP = False
-            card2 = sorted(card_count, key=lambda x: card_count[x], reverse=True)[1]
-            if card_count[card2] == 2:
-                tiers["2P"].append((hand, int(bid)))
-            else:
-                tiers["1P"].append((hand, int(bid)))
-        else:
-            tiers["HC"].append((hand, int(bid)))
-    for tier in ["5", "4", "FH", "3", "2P", "1P", "HC"]:
-        ranked += sort_tier(tiers[tier])
-    ranked.reverse()
-    score = 0
-    for idx, hand in enumerate(ranked):
-        score += (idx + 1) * hand[1]
-    return score
+    return do_it(input_filename)
 
 
 def part_two(input_filename):
-    input = helpers.parse_input(input_filename)
-    ranked = []
-    tiers = {"5": [], "4": [], "FH": [], "3": [], "2P": [], "1P": [], "HC": []}
-    for line in input:
-        hand = line.split()[0]
-        card_count = Counter(line.split()[0])
-        bid = int(line.split()[1])
-        sorted_cards = sorted(card_count, key=lambda x: card_count[x], reverse=True)
-        # sorted_cards = sorted(sorted_cards, key=lambda x: x == 'J')
-        card = sorted_cards[0]
-        if "J" in card_count:
-            if card != "J":
-                card_count[card] += card_count["J"]
-            else:  # card == J
-                try:
-                    card = sorted_cards[1]
-                    card_count[card] += card_count["J"]
-                    card_count["J"] = 0
-                except IndexError:  #
-                    pass
-        if card_count[card] == 5:
-            tiers["5"].append((hand, int(bid)))
-        elif card_count[card] == 4:
-            tiers["4"].append((hand, int(bid)))
-        elif card_count[card] == 3:
-            fh = False
-            for card2 in card_count:
-                if card_count[card2] == 2:
-                    tiers["FH"].append((hand, int(bid)))
-                    fh = True
-            if not fh:
-                tiers["3"].append((hand, int(bid)))
-        elif card_count[card] == 2:
-            card2 = sorted(card_count, key=lambda x: card_count[x], reverse=True)[1]
-            if card_count[card2] == 2:
-                tiers["2P"].append((hand, int(bid)))
-            else:
-                tiers["1P"].append((hand, int(bid)))
-        else:
-            tiers["HC"].append((hand, int(bid)))
-    for tier in ["5", "4", "FH", "3", "2P", "1P", "HC"]:
-        tiers[tier] = sort_tier2(tiers[tier])
-        ranked += sort_tier2(tiers[tier])
-    ranked.reverse()
-    score = sum([(idx + 1) * hand[1] for idx, hand in enumerate(ranked)])
-    return score
+    return do_it(input_filename, part_2=True)
 
 
 if __name__ == "__main__":
