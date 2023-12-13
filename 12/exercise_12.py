fc8caca26ea7b7baf9f45e942853ad053a2d3ca4
Derek Lettman <dlettman@auditboard.com>
2023-12-12 22:52:21 -0800

    solve day 13 ugly

--- 12/exercise_12.py
+++ 12/exercise_12.py
@@ -137,14 +137,14 @@ def part_two(input_filename):
 
 
 if __name__ == "__main__":
-    # print("*** PART ONE ***\n")
-    # print(f"Test result = {part_one('inputtest.txt')}\n")
-    # onestart = time.time()
-    # p1result = part_one("input.txt")
-    # oneend = time.time()
-    # print(f"REAL RESULT = {p1result}")
-    # print(f"Time = {oneend - onestart}")
-    # print("\n")
+    print("*** PART ONE ***\n")
+    print(f"Test result = {part_one('inputtest.txt')}\n")
+    onestart = time.time()
+    p1result = part_one("input.txt")
+    oneend = time.time()
+    print(f"REAL RESULT = {p1result}")
+    print(f"Time = {oneend - onestart}")
+    print("\n")
     print("*** PART TWO ***\n")
     print(f"Test result = {part_two('inputtest.txt')}\n")
     twostart = time.time()
@@ -152,7 +152,7 @@ if __name__ == "__main__":
     twoend = time.time()
     print(f"REAL RESULT = {p2result}")
     print(f"Time = {twoend - twostart}")
-    # if p1result:
-    #     pyperclip.copy(p1result)
-    # elif p2result:
-    #     pyperclip.copy(p2result)
+    if p1result:
+        pyperclip.copy(p1result)
+    elif p2result:
+        pyperclip.copy(p2result)
