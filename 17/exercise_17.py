a648756457a50e2672dc73b6e8f63feb21372aae
Derek Lettman <dlettman@auditboard.com>
2023-12-18 11:19:19 -0800

    clean up day 17

--- 17/exercise_17.py
+++ 17/exercise_17.py
@@ -1,12 +1,10 @@
+import heapq
 import time
+from collections import namedtuple
 
 import pyperclip
 
 from helpers import helpers
-
-import heapq
-
-from collections import namedtuple
 
 
 def create_grid(puzzle_input, max_moves=3):
@@ -22,9 +20,8 @@ def create_grid(puzzle_input, max_moves=3):
 Move = namedtuple("Move", ["risk", "x", "y", "last_dir", "moves_in_that_dir"])
 
 
-def part_one(input_filename):
-    puzzle_input = helpers.parse_input(input_filename)
-    grid = create_grid(puzzle_input)
+def everybody_do_the_dijkstra(puzzle_input, max_moves=3, min_moves=0):
+    grid = create_grid(puzzle_input, max_moves=max_moves)
     max_x = len(puzzle_input[0])
     max_y = len(puzzle_input)
     grid = {key: None for key in grid.keys()}
@@ -33,8 +30,11 @@ def part_one(input_filename):
     while True:
         risk, x, y, last_dir, moves_in_that_dir = heapq.heappop(queue)
         for delta_x, delta_y in helpers.NEIGHBORS_ORTH:
-            if (moves_in_that_dir == 3 and (last_dir == (delta_x, delta_y))) or (last_dir == (-1 * delta_x, -1 * delta_y)):  # disqualify quadruple moves
+            if (moves_in_that_dir == max_moves and (last_dir == (delta_x, delta_y))) or (last_dir == (-1 * delta_x, -1 * delta_y)):
                 continue
+            if min_moves:
+                if (1 <= moves_in_that_dir <= (min_moves - 1) and last_dir != (delta_x, delta_y)):
+                    continue
             new_x, new_y = x + delta_x, y + delta_y
             dir = (delta_x, delta_y)
             num_moves = moves_in_that_dir + 1 if (last_dir == dir) else 1
@@ -44,40 +44,21 @@ def part_one(input_filename):
                 and not grid[(new_x, new_y, dir, num_moves)]
             ):
                 grid[(new_x, new_y, dir, num_moves)] = risk + int(puzzle_input[new_y][new_x])
-                if (new_x, new_y) == (max_x - 1, max_y - 1):
+                if ((new_x, new_y) == (max_x - 1, max_y - 1)) and (not min_moves or (min_moves <= num_moves <= max_moves)):
                     return risk + int(puzzle_input[new_y][new_x])
                 heapq.heappush(
-                    queue, (grid[(new_x, new_y, dir, num_moves)], new_x, new_y, dir, num_moves)
+                    queue, Move(grid[(new_x, new_y, dir, num_moves)], new_x, new_y, dir, num_moves)
                 )
+
+
+def part_one(input_filename):
+    puzzle_input = helpers.parse_input(input_filename)
+    return everybody_do_the_dijkstra(puzzle_input, max_moves=3, min_moves=0)
 
 
 def part_two(input_filename):
     puzzle_input = helpers.parse_input(input_filename)
-    grid = create_grid(puzzle_input, max_moves=10)
-    max_x = len(puzzle_input[0])
-    max_y = len(puzzle_input)
-    grid = {key: None for key in grid.keys()}
-    grid[(0, 0, (0, 0), 0)] = 0  # x, y, last dir, num moves in that dir
-    queue = [Move(0, 0, 0, None, 0)]
-    while True:
-        risk, x, y, last_dir, moves_in_that_dir = heapq.heappop(queue)
-        for delta_x, delta_y in helpers.NEIGHBORS_ORTH:
-            if (moves_in_that_dir == 10 and (last_dir == (delta_x, delta_y))) or (last_dir == (-1 * delta_x, -1 * delta_y)) or (1 <= moves_in_that_dir <= 3 and last_dir != (delta_x, delta_y)):
-                continue
-            new_x, new_y = x + delta_x, y + delta_y
-            dir = (delta_x, delta_y)
-            num_moves = moves_in_that_dir + 1 if (last_dir == dir) else 1
-            if (
-                0 <= new_x < max_x
-                and 0 <= new_y < max_y
-                and not grid[(new_x, new_y, dir, num_moves)]
-            ):
-                grid[(new_x, new_y, dir, num_moves)] = risk + int(puzzle_input[new_y][new_x])
-                if ((new_x, new_y) == (max_x - 1, max_y - 1)) and (4 <= num_moves <= 10):
-                    return risk + int(puzzle_input[new_y][new_x])
-                heapq.heappush(
-                    queue, (grid[(new_x, new_y, dir, num_moves)], new_x, new_y, dir, num_moves)
-                )
+    return everybody_do_the_dijkstra(puzzle_input, max_moves=10, min_moves=4)
 
 
 if __name__ == "__main__":
